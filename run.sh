f8dda5838a05c6b622b001da872ee06d771f84d8
Dave Bray <david@brayworth.com.au>
2022-03-28 09:30:30 +1000

    look harder for PHP

--- run.sh
+++ run.sh
@@ -3,7 +3,10 @@
 WD=`pwd`
 PORT=8080
 
+php=php
+if [[ -x /usr/bin/php8 ]]; then php=php8; fi
+
 cd www
 echo "this application is available at http://localhost:$PORT"
-php -S localhost:$PORT _mvp.php
+$php -S localhost:$PORT _mvp.php
 cd $WD
